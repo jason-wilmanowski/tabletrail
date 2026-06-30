11d1cceb98c80e8d304db6989c7977aedd5a01aa
Jason Wilmanowski <jasonwilmanowski23@gmail.com>
2026-06-30 15:05:00 +0200

    feat: add columns db model

---
+++ table_trail_backend/db/models/columns.py
@@ -0,0 +1,20 @@
+from sqlalchemy import UniqueConstraint
+from sqlalchemy.orm import Mapped, mapped_column
+from table_trail_backend.db.database_config import Base
+
+
+class Columns(Base):
+    __tablename__ = 'columns'
+
+    id : Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    table_id : Mapped[int] = mapped_column(primary_key=True)
+    name : Mapped[str] = mapped_column()
+    data_type : Mapped[str] = mapped_column()
+    is_nullable : Mapped[bool] = mapped_column()
+    default_value : Mapped[str] = mapped_column()
+    ordinal_position : Mapped[int] = mapped_column()
+
+    __table_args__ = (
+        UniqueConstraint('table_id', 'name', name='uq_table_per_db_schema'),
+    )
+
