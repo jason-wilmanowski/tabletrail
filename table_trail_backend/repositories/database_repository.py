1df659d971c02b23ef1fdd8ad98d15a6ee136a54
Jason Wilmanowski <jasonwilmanowski23@gmail.com>
2026-07-03 11:49:59 +0200

    feat: implement TableRepository with CRUD operations

--- table_trail_backend/repositories/database_repository.py
+++ table_trail_backend/repositories/database_repository.py
@@ -2,6 +2,8 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select, delete
 from table_trail_backend.db.models.databases import Databases
 from table_trail_backend.schemas.database_schema import CreateDatabase, UpdateDatabase
+
+
 class DatabasesRepository:
 
 
---
+++ table_trail_backend/repositories/table_repository.py
@@ -0,0 +1,50 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select, delete, and_
+from table_trail_backend.db.models.tables import Tables
+
+
+
+class TableRepository:
+
+    def __init__(self, session: AsyncSession):
+
+        self.db = session
+
+
+    async def create_table(self, db_id: int, name: str, schema_name: str):
+        new_table = Tables(database_id=db_id, name=name, schema_name=schema_name)
+        self.db.add(new_table)
+        await self.db.flush()
+        await self.db.refresh(new_table)
+        return new_table
+
+
+    async def get_table_by_id(self, db_id: int, table_id: int):
+        table = await self.db.execute(
+            select(Tables).where(and_(Tables.id == table_id,
+                                      Tables.database_id == db_id))
+        )
+        return table.scalar_one_or_none()
+
+    async def get_database_tables(self, db_id: int):
+        tables = await self.db.execute(
+            select(Tables).where(Tables.database_id == db_id))
+        return tables.scalars().all()
+
+
+    async def update_table(self, db_id: int, table_id: int, name: str | None = None, schema_name: str | None = None):
+        database = await self.get_table_by_id(db_id, table_id)
+        if name is not None:
+            database.name = name
+        if schema_name is not None:
+            database.schema_name = schema_name
+        await self.db.flush()
+        await self.db.refresh(database)
+        return database
+
+
+    async def delete_table(self, db_id: int, table_id: int):
+        database = await self.get_table_by_id(db_id, table_id)
+        await self.db.delete(database)
+        await self.db.flush()
+        return
