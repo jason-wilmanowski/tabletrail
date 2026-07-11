365dce7e1375608f94ae265a997b500e16d7bd16
Jason Wilmanowski <jasonwilmanowski23@gmail.com>
2026-07-11 12:09:45 +0200

    feat: implemented general database service

---
+++ table_trail_backend/services/database_service.py
@@ -0,0 +1,36 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+from table_trail_backend.repositories.database_repository import DatabasesRepository
+from table_trail_backend.core.exceptions import DatabaseError
+from table_trail_backend.schemas.database_schema import UpdateDatabase
+
+
+class DatabasesService:
+
+
+    def __init__(self, db: AsyncSession):
+        self.db = db
+        self.db_repo = DatabasesRepository(db)
+
+    async def get_full_database(self, db_id: int):
+        database = await self.db_repo.get_full_database(db_id)
+        if database is None:
+            raise DatabaseError(f"Database with id {db_id} not found")
+        return database
+
+    async def get_all_databases(self):
+        return await self.db_repo.get_all_databases()
+
+    async def update_database(self, db_id: int, data: UpdateDatabase):
+        if not data:
+            raise DatabaseError("No data to update")
+        database = await self.db_repo.get_one_database(db_id)
+        if database is None:
+            raise DatabaseError(f"Database with id {db_id} not found")
+        return await self.db_repo.update(db_id, data)
+
+    async def delete_database(self, db_id: int):
+        database = await self.db_repo.get_one_database(db_id)
+        if database is None:
+            raise DatabaseError(f"Database with id {db_id} not found")
+        await self.db_repo.delete_database(db_id)
+        await self.db.commit()
