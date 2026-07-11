fb6278feb44cfc8ce48806bb45143b939ccf7c6e
Jason Wilmanowski <jasonwilmanowski23@gmail.com>
2026-07-11 14:15:54 +0200

    feat: implemented database creation endpoint -> returning full database structure

---
+++ table_trail_backend/api/scanner.py
@@ -0,0 +1,35 @@
+from fastapi import APIRouter, HTTPException, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+from table_trail_backend.schemas.database_schema import DatabaseResponse, CreateDatabase
+from table_trail_backend.core.dependencies import get_db
+from table_trail_backend.services.scanner_service import ScanService
+from table_trail_backend.services.database_service import DatabaseService
+from table_trail_backend.core.exceptions import ScanningSystemError, DatabaseSystemError
+
+router = APIRouter(prefix="/scanner", tags=["scanner"])
+
+
+@router.post("", response_model=DatabaseResponse)
+async def create_database(database_details: CreateDatabase, db: AsyncSession = Depends(get_db)) -> DatabaseResponse:
+
+    scanner_service = ScanService(db)
+    database_service = DatabaseService(db)
+
+    try:
+        # call service to scan db via database details
+        database = await scanner_service.execute_scan(database_details)
+    except ScanningSystemError as error:
+        raise HTTPException(status_code=error.status_code, detail=error.message)
+
+    try:
+        # get full database structure via database id
+        database_structure = await database_service.get_full_database(database.id)
+    except DatabaseSystemError as error:
+        raise HTTPException(status_code=error.status_code, detail=error.message)
+
+    return database_structure
+
+
+
+
+
