85d64a65150a03162513196319b29f698390b2d9
Jason Wilmanowski <jasonwilmanowski23@gmail.com>
2026-09-01 16:55:31 +0200

    feat(service): add column relation service class

--- table_trail_backend/db/models/column_relations.py
+++ table_trail_backend/db/models/column_relations.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Enum, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column
 
-from table_trail_backend.core.enums import ColumnRelationType
+from table_trail_backend.core.enums import ColumnRelationColor
 from table_trail_backend.db.database_config import Base
 
 
@@ -12,7 +12,7 @@ class ColumnRelations(Base):
     database_id: Mapped[int] = mapped_column(ForeignKey("databases.id", ondelete="CASCADE", onupdate="CASCADE"))
     column_id_1: Mapped[int] = mapped_column(ForeignKey("columns.id", ondelete="CASCADE"))
     column_id_2: Mapped[int] = mapped_column(ForeignKey("columns.id", ondelete="CASCADE"))
-    relation_color: Mapped[ColumnRelationType] = mapped_column(
-        Enum(ColumnRelationType), default=ColumnRelationType.GREEN
+    relation_color: Mapped[ColumnRelationColor] = mapped_column(
+        Enum(ColumnRelationColor), default=ColumnRelationColor.GREEN
     )
     description: Mapped[str | None] = mapped_column()
