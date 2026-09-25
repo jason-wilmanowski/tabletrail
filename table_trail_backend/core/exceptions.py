715f1f5de8b39d30d8209f4bb09a25e277769c0d
Jason Wilmanowski <jasonwilmanowski23@gmail.com>
2026-09-25 16:24:26 +0200

    feature(encrypting): add enrypting file + custom exceptions

--- table_trail_backend/core/exceptions.py
+++ table_trail_backend/core/exceptions.py
@@ -89,3 +89,20 @@ class ExportPdfError(ExportSystemError):
 
 class ExportMarkdownError(ExportSystemError):
     pass
+
+
+# Base Encryption Exception
+class EncryptionSystemError(Exception):
+    def __init__(self, message: str, status_code: int):
+        self.message = message
+        self.status_code = status_code
+        super().__init__(message)
+
+
+# Encryption Errors
+class EncryptionKeyError(EncryptionSystemError):
+    pass
+
+
+class EncryptionError(EncryptionSystemError):
+    pass
